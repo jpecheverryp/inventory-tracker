5e488e774e41ab7b3495ad2c3ed35fed7640a6b7
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-05-31 16:04:35 -0400

    added bcrypt for encryption on user creation

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
 
 const PORT = 3000;
 
 app.use(express.json())
 
-const users = [
-    {
-        username: "John",
-        password: "password1"
-    },
-    {
-        username: "Kelly",
-        password: "password2"
-    },
-    {
-        username: "Pablo",
-        password: "password3"
-    }
-]
+const users = []
 
 app.get('/', (req, res) => {
     res.send("hello world")
@@ -27,16 +15,23 @@ app.get('/users', (req, res) => {
     res.status(200)
     res.json(users)
 })
-app.post('/users', (req, res) => {
-    const newUser = {
-        username: req.body.username,
-        password: req.body.password
+app.post('/users', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const newUser = {
+            username: req.body.username,
+            password: hashedPassword
+        }
+        users.push(newUser)
+        res.status(200)
+        res.json({ message: "User created" })
+    } catch (err) {
+        res.status(500)
+        res.json(err)
     }
-    users.push(newUser)
-    res.status(200)
-    res.json({message: "User created"})
+
 })
 
 app.listen(PORT, () => {
-  console.log("Express App listening at http://localhost:" + PORT);  
+    console.log("Express App listening at http://localhost:" + PORT);
 })
