c35ce0e67369c54af0da356e489d835db8de0223
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-05-31 15:49:20 -0400

    added users fake data on server

--- server.js
+++ server.js
@@ -3,8 +3,27 @@ const app = express();
 
 const PORT = 3000;
 
+const users = [
+    {
+        username: "John",
+        password: "password1"
+    },
+    {
+        username: "Kelly",
+        password: "password2"
+    },
+    {
+        username: "Pablo",
+        password: "password3"
+    }
+]
+
 app.get('/', (req, res) => {
     res.send("hello world")
+})
+app.get('/users', (req, res) => {
+    res.status(200)
+    res.json(users)
 })
 
 app.listen(PORT, () => {
